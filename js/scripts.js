c7e450bbbbd4684a0a3b47c34bf73771fe42b66e
Aziz Kemal HOSCAN <kemal.hoscan@yahoo.com>
2020-02-20 18:33:53 +0000

    implemented modal function and hided search bar

--- js/scripts.js
+++ js/scripts.js
@@ -15,27 +15,29 @@ fetch('https://randomuser.me/api/?results=12')
 // ----------------------------------------------------------------
 // Here I selected the search container division and created dom elements by following
 // search markup specified as comment in hmtl file.
-// YOU MAY WANT TO ALTER THIS!
-  const searchContainer = document.querySelector('.search-container');
-  // console.log(searchContainer);
-  const html = document.createElement('FORM');
-  let inputElement1 = document.createElement('INPUT')
-  inputElement1.type = "search";
-  inputElement1.id = "search-input";
-  inputElement1.classList = "search-input";
-  inputElement1.placeholder = "Search...";
-  html.appendChild(inputElement1);
-  let inputElement2 = document.createElement('INPUT')
-  inputElement2.type = "submit";
-  inputElement2.value = "Randomize";
-  inputElement2.classList = "search-submit";
-  inputElement2.id = "search-submit";
-  html.appendChild(inputElement2);
-searchContainer.appendChild(html);
+// YOU MAY HIDE THIS PART
+//   const searchContainer = document.querySelector('.search-container');
+//   // console.log(searchContainer);
+//   const html = document.createElement('FORM');
+//   let inputElement1 = document.createElement('INPUT')
+//   inputElement1.type = "search";
+//   inputElement1.id = "search-input";
+//   inputElement1.classList = "search-input";
+//   inputElement1.placeholder = "Search...";
+//   html.appendChild(inputElement1);
+//   let inputElement2 = document.createElement('INPUT')
+//   inputElement2.type = "submit";
+//   inputElement2.value = "Randomize";
+//   inputElement2.classList = "search-submit";
+//   inputElement2.id = "search-submit";
+//   html.appendChild(inputElement2);
+// searchContainer.appendChild(html);
 // ---------------------------------------------------------------
 
 // Division containg cards created and added its inner html which is dynamic.
-
+// Looped 12 times since we are expected to do so..
+// Followed the design mockup instructions in html file.
+// Also set ids to index of objects..
 function generateCards(data){
   const addhere = document.querySelector('#gallery');
   for(let i = 0; i < 12; i++){
@@ -55,6 +57,9 @@ function generateCards(data){
   }
 }
 // -----------------------------------------------------------------
+// Here I created a function in order to create the modal from the targeted card.
+// I took index by checking its id - which i defined in the previous function..
+// Used index to match the card with the Modal pops out when card clicked..
 function generateModal(data){
   const  bodyElement = document.getElementsByTagName("BODY");
   const newDiv = document.createElement('DIV');
@@ -65,21 +70,25 @@ function generateModal(data){
   selectCard.addEventListener('click', (e) => {
     console.log('I am generateModal function');
     // console.log(bodyElement);
-    // --------------------HEREEEEEEEEEEEEE------------------
-    console.log(e.target.id);
+
+    const dataArray = data.results
+    console.log(dataArray);
+    let indexOfTargetE = e.target.id;
+    let integer = parseInt(indexOfTargetE, 10);
+    console.log(integer);
   const html = `
   <div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
+                        <img class="modal-img" src="${data.results[integer].picture.thumbnail}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${data.results[integer].name.first}</h3>
+                        <p class="modal-text">${data.results[integer].email}</p>
+                        <p class="modal-text cap">${data.results[integer].location.city}</p>
                         <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
+                        <p class="modal-text">${data.results[integer].cell}</p>
+                        <p class="modal-text"> ${data.results[integer].location.street.name} ${data.results[integer].location.street.number}, ${data.results[integer].location.state}, ${data.results[integer].location.postcode}</p>
+                        <p class="modal-text">Birthday: ${data.results[integer].dob.date.slice(8,10)}/${data.results[integer].dob.date.slice(5,7)}/${data.results[integer].dob.date.slice(0,4)}</p>
                     </div>
                 </div>
   </div>
@@ -96,16 +105,7 @@ function generateModal(data){
   })
   })
 }
-
 // ------------------------------------------------------------------
-// Select elements here
-
-// var searchContainerElement = $('.search-container');
-// var galleryDiv = $('#gallery');
-// var header = $('.header-text-container');
-
-// ------------------------------------------------------------------
-
 function checkStatus(response){
   // You do not need to equalize this to true since it is a value either true or false/
   if(response.ok){
